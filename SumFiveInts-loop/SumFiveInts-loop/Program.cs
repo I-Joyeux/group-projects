5fe8be978b83b406a936444836aec162ca6b91da
I-Joyeux <143741304+I-Joyeux@users.noreply.github.com>
2023-09-18 15:09:12 -0600

    looping a user to write 5num and adding them up

---
+++ SumFiveInts-loop/SumFiveInts-loop/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int total = 0;
+int sum = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Enter a number >> ");
+    int num=Convert.ToInt32(Console.ReadLine());
+
+    sum =total += num;
+  
+
+  ;
+}
+Console.WriteLine(sum);
