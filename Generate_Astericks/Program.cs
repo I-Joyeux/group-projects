c28c4c0ed43cd66deabe289113d963f6be50469c
I-Joyeux <143741304+I-Joyeux@users.noreply.github.com>
2023-09-18 11:59:36 -0600

    printing stars that a user asked
    We ask the use how many stars they wan to print and they write the integer and we print the amount of stars

---
+++ Generate_Astericks/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+//while (answer=='y' ||answer=='Y')
+//{
+
+//while ()
+//{ 
+
+//}
+// Console.Write("Do oyu want to generate another set of astericks? Y or N");
+//   char answer=Convert.ToChar(Console.ReadLine());
+//}
+char astreriks = Convert.ToChar('*');
+Console.Write("Enter the number of astericks  ");
+int amount = Convert.ToInt32(Console.ReadLine());
+
+while (true)
+{
+    Console.Write("Do you want to generate another set of astericks? Y or N  ");
+    char answer = Convert.ToChar(Console.ReadLine());
+
+    while (answer == 'y' || answer == 'Y')
+    {
+
+        for (int i = 0; i <= amount; i++)
+        {
+
+            Console.Write(astreriks);
+
+        }
+        Console.WriteLine("Do oyu want to generate another set of astericks? Y or N");
+        answer = Convert.ToChar(Console.ReadLine());
+        amount = Convert.ToInt32(Console.ReadLine());
+        astreriks = Convert.ToChar('*');
+    }
+    Console.WriteLine("bye");
+}
+
